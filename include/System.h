5183999d60b2a08f3ce9d34d071fd408a29e5c82
marc haubenstock <marc.haubenstock@ait.ac.at>
2024-02-27 08:06:35 +0000

    fixed bug

--- include/System.h
+++ include/System.h
@@ -142,7 +142,7 @@ public:
     bool isShutDown();
 
     // Returns image of the current frame being tracked with features
-    cv::Mat System::DrawTrackedImage();
+    cv::Mat DrawTrackedImage();
 
     // Save camera trajectory in the TUM RGB-D dataset format.
     // Only for stereo and RGB-D. This method does not work for monocular.
