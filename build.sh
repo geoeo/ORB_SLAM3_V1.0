fac5f73bc268ef13c5efd9f690b6e95ac718c6d6
marc haubenstock <marc.haubenstock@ait.ac.at>
2024-03-01 08:24:06 +0000

    debugging

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "Configuring and building Thirdparty/DBoW2 ..."
 cd Thirdparty/DBoW2
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=${1:-Release}
 make -j
 
 cd ../../g2o
@@ -13,7 +13,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=${1:-Release}
 make -j
 
 cd ../../Sophus
@@ -22,7 +22,7 @@ echo "Configuring and building Thirdparty/Sophus ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=${1:-Release}
 make -j
 
 cd ../../../
@@ -37,5 +37,5 @@ echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=${1:-Release}
 make -j4
