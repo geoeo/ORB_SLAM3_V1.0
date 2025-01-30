5e1a468ace4def3b26f0df9459fe0bf2ec4af13b
Marc Haubenstock <marc.haubenstock@ait.ac.at>
2025-01-30 08:08:56 +0000

    launch for orin

--- ros2_ws/src/mono-inertial/launch/launch_bag.py
+++ ros2_ws/src/mono-inertial/launch/launch_bag.py
@@ -4,11 +4,9 @@ import launch.actions
 import launch_ros.actions
 
 bag_start_offset = 200.0
-#bag_path = '/workspaces/bags/20240222_Günselsdorf_Eve_IMU/tegra_master_2024-02-22-09-34-02'
-#bag_path = '/bags/openrealm/20240229_Günselsdorf_Eve_IMU/tegra_master_2024-02-29-09-54-11'
-bag_path = '/bags/openrealm/2024_07_25-Guenselsdorf/flight1/bags/all/rosbag2_2024_07_25-09_27_00'
+bag_path = '/bags/rosbag2_2024_09_25-11_04_09_uncompressed_fighter_4/rosbag2_2024_09_26-06_55_33'
 
-image_topic = '/AIT_Fighter5/down/image'
+image_topic = '/AIT_Fighter4/down/image'
 
 def generate_launch_description():
     return LaunchDescription([
@@ -16,20 +14,8 @@ def generate_launch_description():
             cmd=['ros2', 'run', 'mono-inertial', 'mono_inertial_node', '/workspaces/ORB_SLAM3_V1.0/Vocabulary/ORBvoc.txt', 'false'],
             output='screen'
         ),
-        launch_ros.actions.Node(
-            package='image_transport',
-            executable='republish',
-            name='im_transport',
-            output='log',
-            arguments=['compressed', 'raw'],
-            remappings=[
-                ('in/compressed',f'{image_topic}/compressed'),
-                ('out',f'{image_topic}')
-            ]
-        ),
         launch.actions.ExecuteProcess(
-            cmd=['ros2', 'bag', 'play', f'{bag_path}', '--start-offset',f'{bag_start_offset}', '--topics', f'{image_topic}/compressed', '/bmi088/imu',
-],
+            cmd=['ros2', 'bag', 'play', f'{bag_path}', '--start-offset',f'{bag_start_offset}', '--topics', f'{image_topic}', '/bmi088_F4/imu'],
             output='log'
         )
 ])
