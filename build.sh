456ba532237829176079af041cff7d2878402354
Marc Haubenstock <marc.haubenstock@ait.ac.at>
2025-02-06 15:46:47 +0000

    reverted build options

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ cd ../g2o
 
 echo "Configuring and building Thirdparty/g2o ..."
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=/usr/local -DG2O_BUILD_APPS=OFF -DG2O_BUILD_EXAMPLES=OFF -DG2O_USE_OPENGL=OFF
+cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=/usr/local -DG2O_BUILD_APPS=OFF -DBUILD_WITH_MARCH_NATIVE=OFF -DG2O_BUILD_EXAMPLES=OFF -DG2O_USE_OPENGL=OFF
 cmake --build build -j $(nproc --all) --target install
 
 cd ../Sophus
@@ -31,5 +31,5 @@ cd ..
 
 echo "Configuring and building ORB_SLAM3 ..."
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DTRACY_ENABLE=OFF "$@"
+cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DTRACY_ENABLE=ON "$@"
 cmake --build build -j 6 --target install
