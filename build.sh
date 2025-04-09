1bcde93be75f9d8946dbcf71a69dea772eb6b78a
Marc Haubenstock <marc.haubenstock@ait.ac.at>
2025-04-09 14:12:28 +0000

    disable g2o native in x86

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ cd ../g2o
 
 echo "Configuring and building Thirdparty/g2o ..."
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=/usr/local -DG2O_BUILD_APPS=OFF -DBUILD_WITH_MARCH_NATIVE=ON -DG2O_BUILD_EXAMPLES=OFF -DG2O_USE_OPENGL=OFF
+cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=/usr/local -DG2O_BUILD_APPS=OFF -DBUILD_WITH_MARCH_NATIVE=OFF -DG2O_BUILD_EXAMPLES=OFF -DG2O_USE_OPENGL=OFF
 cmake --build build -j $(nproc --all) --target install
 
 cd ../Sophus
