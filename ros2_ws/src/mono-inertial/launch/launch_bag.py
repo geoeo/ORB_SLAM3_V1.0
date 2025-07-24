d98b4d9a4c87330113b0e0cfe05d1049f69699a8
Marc Haubenstock <marc.haubenstock@ait.ac.at>
2025-07-24 14:51:40 +0000

    launch

--- ros2_ws/src/mono-inertial/launch/launch_bag.py
+++ ros2_ws/src/mono-inertial/launch/launch_bag.py
@@ -3,12 +3,14 @@ from launch import LaunchDescription
 import launch.actions
 import launch_ros.actions
 
-bag_start_offset = 180.0
+bag_start_offset = 120.0
 #bag_path = '/bags/240925_guenselsdorf/fighter4/2024_09_25-guenselsdorf/seq0/bags/all/rosbag2_2024_09_25-11_04_09'
-bag_path = '/bags/2025_03_04-Steinalpl-rec/seq0/bags/all/rosbag2_2025_03_04-11_31_57/'
+#bag_path = '/bags/2025_03_04-Steinalpl-rec/seq0/bags/all/rosbag2_2025_03_04-11_31_57/'
+#bag_path = '/bags/2025_05_15-Calvarina-rec/seq0/bags/all/rosbag2_2025_05_15-08_25_08_circle_110m'
+bag_path = '/bags/2025_05_15-Calvarina-rec/seq0/bags/all/rosbag2_2025_05_15-09_14_04_grid_80m'
 
-air_id = 5
-rate = 1.0
+air_id = 6
+rate = 0.1
 image_topic = f'/AIT_Fighter{air_id}/down/image'
 
 def generate_launch_description():
@@ -31,7 +33,7 @@ def generate_launch_description():
         launch.actions.ExecuteProcess(
             cmd=['ros2', 'bag', 'play', f'{bag_path}', 
             '--start-offset',f'{bag_start_offset}', 
-            '--topics',f'{image_topic}', f'{image_topic}/compressed', f'/bmi088_F{air_id}/imu',
+            '--topics', f'{image_topic}/compressed', f'/bmi088_F{air_id}/imu',
             '--rate', f'{rate}'],
             output='log'
         )
