81a51de31bc66953e5c32041dcd90c254ab5f366
Marc Haubenstock <marc.haubenstock@ait.ac.at>
2025-08-06 10:48:43 +0000

    changed node settings

--- ros2_ws/src/mono-inertial/src/mono_inertial_node.cpp
+++ ros2_ws/src/mono-inertial/src/mono_inertial_node.cpp
@@ -275,7 +275,7 @@ class SlamNode : public rclcpp::Node
 
       //F6
 
-      m_imu.accelWalk  = 0.007579860836224204; //x2000
+      m_imu.accelWalk  = 0.07579860836224204; //x20000
       m_imu.gyroWalk   = 0.00000352677789580351; //x2
       m_imu.noiseAccel =  0.03138444640779682; //x200
       m_imu.noiseGyro  = 0.0003278894143880944; // x2
