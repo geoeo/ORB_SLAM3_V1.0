4c76fab67c46064d1cd297f0040d488d2b9ff744
Marc Haubenstock <marc.haubenstock@ait.ac.at>
2025-10-23 11:46:10 +0000

    added new bags

--- ros2_ws/src/mono-inertial/launch/launch_gnss_bag.py
+++ ros2_ws/src/mono-inertial/launch/launch_gnss_bag.py
@@ -3,20 +3,40 @@ from launch import LaunchDescription
 import launch.actions
 import launch_ros.actions
 
-bag_start_offset = 60.0
-#bag_path = '/bags/240925_guenselsdorf/fighter4/2024_09_25-guenselsdorf/seq0/bags/all/rosbag2_2024_09_25-11_04_09'
+bag_start_offset =150.0
+
+
 #bag_path = '/bags/2025_03_04-Steinalpl-rec/seq0/bags/all/rosbag2_2025_03_04-11_31_57/'
 #bag_path = '/bags/2025_05_15-Calvarina-rec/seq0/bags/all/rosbag2_2025_05_15-08_25_08_circle_110m'
 #bag_path = '/bags/2025_05_15-Calvarina-rec/seq0/bags/all/rosbag2_2025_05_15-09_14_04_grid_80m'
 #bag_path = '/bags/2025_08_13-Streitdorf-rec/seq0/bags/all/rosbag2_2025_08_13-09_38_53_25fps'
 #bag_path = '/bags/2025_08_14-Streitdorf-rec/seq0/bags/all/rosbag2_2025_08_14-08_30_32_15fps'
 #bag_path = '/bags/2025_09_22-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_09_22-09_19_35_10hz'
+bag_path = '/bags/2025_10_15-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_15-09_49_48_eight' # 150
+#bag_path = '/bags/2025_10_15-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_15-10_05_51_eight_long'
+#bag_path = '/bags/2025_10_15-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_15-10_53_50_ellipse_lang_gegen'
+#bag_path = '/bags/2025_10_15-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_15-11_39_27_ellipse_lang_im'
+#bag_path = '/bags/2025_10_15-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_15-12_06_43_large_circle'
+#bag_path = '/bags/2025_10_15-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_15-12_17_58_ellpise_gegen'
+#bag_path = '/bags/2025_10_15-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_15-12_25_31_ellpise_im'
+#bag_path = '/bags/2025_10_15-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_15-12_35_55_eight_heigh_with_ratio'
+
+#bag_path = '/bags/2025_10_22-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_22-09_20_38_eight_straight' # 100
+#bag_path = '/bags/2025_10_22-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_22-09_36_18_straight_eight' # 100
+#bag_path = '/bags/2025_10_22-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_22-10_12_22_eight_circle' # 100
+#bag_path = '/bags/2025_10_22-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_22-10_21_21_circle_eight' # 100
+#bag_path = '/bags/2025_10_22-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_22-10_34_26_eight_eight_high' # 100
+#bag_path = '/bags/2025_10_22-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_22-10_51_54_eight_high_eight' # 100
+#bag_path = '/bags/2025_10_22-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_22-10_59_56_eight_eight_long' # 100
+#bag_path = '/bags/2025_10_22-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_22-11_16_33_eight_long_eight' # 100
+#bag_path = '/bags/2025_10_22-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_10_22-11_33_48_straight_straight' # 100
+
 
 # F4
-bag_path = '/bags/2025_06_06-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_06_06-09_32_00_12fps'
+#bag_path = '/bags/2025_06_06-Guenselsdorf-rec/seq0/bags/all/rosbag2_2025_06_06-09_32_00_12fps'
 
-air_id = 4
-rate = 0.5
+air_id = 6
+rate = 1.0
 image_topic = f'/AIT_Fighter{air_id}/down/image'
 gnss_topic = f'/AIT_Fighter{air_id}/mavros/global_position/global'
 read_ahead_queue_size = 5000
