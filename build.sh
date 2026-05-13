f30ca5eae1210df26858289b0e813c22a03b834c
Marc Haubenstock <marc.dhaubenstock@gmail.com>
2026-05-13 10:51:05 +0200

    Update build.sh
    Removing install from build script

--- build.sh
+++ build.sh
@@ -5,21 +5,21 @@ export G2O_FAST_MATH=${2:-OFF}
 
 cd Thirdparty/DBoW2
 cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_CXX_STANDARD=17
-cmake --build build -j $(nproc --all) --target install
+cmake --build build -j $(nproc --all)
 
 cd ../g2o
 
 echo "Configuring and building Thirdparty/g2o ..."
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_CXX_STANDARD=17 -DG2O_BUILD_APPS=OFF -DG2O_FAST_MATH=${G2O_FAST_MATH} -DBUILD_UNITTESTS=OFF -DG2O_BUILD_EXAMPLES=OFF -DG2O_USE_OPENGL=OFF
-sudo cmake --build build -j $(nproc --all) --target install
+cmake --build build -j $(nproc --all)
 
 cd ../Sophus
 
 echo "Configuring and building Thirdparty/Sophus ..."
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_SOPHUS_TESTS=OFF
-sudo cmake --build build -j $(nproc --all) --target install
+cmake --build build -j $(nproc --all)
 
 cd ../../
 
@@ -32,4 +32,4 @@ cd ..
 echo "Configuring and building ORB_SLAM3 ..."
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DTRACY_ENABLE=OFF
-sudo cmake --build build -j $(nproc --all) --target install
+cmake --build build -j $(nproc --all)
