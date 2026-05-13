90582bee6dec1a914965e1c9042956bbafa09102
Marc Haubenstock <marc.haubenstock@ait.ac.at>
2026-05-13 09:19:59 +0000

    change install to build but made it sudo

--- build_and_install.sh
+++ build_and_install.sh
@@ -6,6 +6,7 @@ export G2O_FAST_MATH=${2:-OFF}
 cd Thirdparty/DBoW2
 cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_CXX_STANDARD=17
 cmake --build build -j $(nproc --all)
+sudo cmake --install build
 
 cd ../g2o
 
@@ -13,6 +14,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_CXX_STANDARD=17 -DG2O_BUILD_APPS=OFF -DG2O_FAST_MATH=${G2O_FAST_MATH} -DBUILD_UNITTESTS=OFF -DG2O_BUILD_EXAMPLES=OFF -DG2O_USE_OPENGL=OFF
 cmake --build build -j $(nproc --all)
+sudo cmake --install build
 
 cd ../Sophus
 
@@ -20,6 +22,7 @@ echo "Configuring and building Thirdparty/Sophus ..."
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_SOPHUS_TESTS=OFF
 cmake --build build -j $(nproc --all)
+sudo cmake --install build
 
 cd ../../
 
@@ -33,3 +36,4 @@ echo "Configuring and building ORB_SLAM3 ..."
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DTRACY_ENABLE=OFF
 cmake --build build -j $(nproc --all)
+sudo cmake --install build
