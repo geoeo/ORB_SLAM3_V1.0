c24bba0591db5de5f1b05bde0c7faa04654f428c
Marc Haubenstock <marc.dhaubenstock@gmail.com>
2026-05-13 10:41:11 +0200

    Update build.sh
    added sudo to install

--- build.sh
+++ build.sh
@@ -12,14 +12,14 @@ cd ../g2o
 echo "Configuring and building Thirdparty/g2o ..."
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_CXX_STANDARD=17 -DG2O_BUILD_APPS=OFF -DG2O_FAST_MATH=${G2O_FAST_MATH} -DBUILD_UNITTESTS=OFF -DG2O_BUILD_EXAMPLES=OFF -DG2O_USE_OPENGL=OFF
-cmake --build build -j $(nproc --all) --target install
+sudo cmake --build build -j $(nproc --all) --target install
 
 cd ../Sophus
 
 echo "Configuring and building Thirdparty/Sophus ..."
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_SOPHUS_TESTS=OFF
-cmake --build build -j $(nproc --all) --target install
+sudo cmake --build build -j $(nproc --all) --target install
 
 cd ../../
 
@@ -32,4 +32,4 @@ cd ..
 echo "Configuring and building ORB_SLAM3 ..."
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DTRACY_ENABLE=OFF
-cmake --build build -j $(nproc --all) --target install
+sudo cmake --build build -j $(nproc --all) --target install
